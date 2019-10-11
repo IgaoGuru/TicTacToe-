36c26db5db959d6a9a1e4b140757646328646788
Igão Guru <47335482+IgaoGuru@users.noreply.github.com>
2019-10-10 23:31:28 -0300

    v6.5 training neural network optimized
    optimization and polishing of nn training

--- nntraining.py
+++ nntraining.py
@@ -5,12 +5,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+import pickle
 
 lr = 0.01
 momentum = 0.5
-epochs = 10
+epochs = 20
 device = "cpu"
-log_interval = 10
+log_interval = 100
 
 class TictactoeNet(nn.Module):
     #defines convolutional and dropout layers as part of NN
@@ -29,13 +30,13 @@ class TictactoeNet(nn.Module):
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         x = F.relu(self.fc4(x))
-        return F.softmax(x, dim = 1)
+        return F.log_softmax(x, dim = 1)
 
 def train(model, device, X, y, optimizer, epoch):
     #tells model its training time so layers like dropout can behave acordingly;
     model.train()
     #for each batch in training dataset ready to be used:
-    batch_size = 1000
+    batch_size = 1
     for idx in range(int(X.shape[0]/batch_size)):
         begin = idx * batch_size
         data = X[begin:begin + batch_size, :]
@@ -47,7 +48,7 @@ def train(model, device, X, y, optimizer, epoch):
         #forward pass
         output = model(data)
         #loss function
-        loss = F.cross_entropy(output, target)
+        loss = F.nll_loss(output, target)
         #backwards propagation
         loss.backward()
         #optimizes all layers
@@ -57,6 +58,32 @@ def train(model, device, X, y, optimizer, epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, idx * len(data), X.shape[0],
                        100. * idx / X.shape[0], loss.item()))
+
+def test(model, device, X, y):
+    #for each batch in training dataset ready to be used:
+    batch_size = 250
+    num_corrects = 0
+    for idx in range(int(X.shape[0]/batch_size)):
+        begin = idx * batch_size
+        data = X[begin:begin + batch_size, :]
+        target = y[begin : begin + batch_size]
+        target = target.reshape((target.shape[0], ))
+        #throw it on cpu or gpu
+        data, target = data.to(device), target.to(device)
+        #forward pass
+        output = model(data)
+        output = output.cpu().detach().numpy()
+        y_pred = np.argmax(output, axis=1).flatten()
+        y_true = y_torch[begin:begin + batch_size].cpu().detach().numpy().flatten()
+        correct = y_pred == y_true
+        num_corrects += np.sum(correct)
+        #logs every 11th batch index (if log_interval == 11)
+        if idx % log_interval == 0:
+            print('Test: [{}/{} ({:.0f}%)]'.format(
+                 idx * len(data), X.shape[0],
+                       100. * idx / X.shape[0]))
+    return num_corrects
+
 y_col = ["optimal_play"]
 
 tictactoe_data_filepath = "tree_exported.txt"
@@ -85,13 +112,20 @@ Y = np.zeros((y.shape[0], 9))
 X_torch = torch.tensor(X).float()
 y_torch = torch.tensor(y).long()
 # Y_torch = torch.tensor(Y).long()
+load_model = False
+if load_model:
+    model = pickle.load(open("TictactoeNet.p", "rb"))
+else:
+    model = TictactoeNet().to(device)
+    output_y = model.forward(X_torch)
 
-model = TictactoeNet().to(device)
-output_y = model.forward(X_torch)
-
-optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
-#trains & tests each epoch
-for epoch in range(1, epochs + 1):
-    train(model, device, X_torch, y_torch, optimizer, epoch)
+    optimizer = optim.Adam(model.parameters())
+    #trains & tests each epoch
+    for epoch in range(1, epochs + 1):
+        train(model, device, X_torch, y_torch, optimizer, epoch)
 
 
+    pickle.dump(model, open("TictactoeNet.p", "wb"))
+
+num_corrects = test(model, device, X_torch, y_torch)
+print(num_corrects/X_torch.shape[0])
